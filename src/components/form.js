59574372e2c168b43a26f423de5d0f38ed97b4c7
lu4ezar <misha-shisha@mail.ru>
2019-04-26 08:36:39 +0700

    tt

--- src/components/form.js
+++ src/components/form.js
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
 
-const Form = props => {
+const Form = () => {
 	const [input, setInput] = useState('');
 	const [list, addItem] = useState([]);
-	const handleChange = () => {
-		setInput(input);
-	}
-	const handleClick = () => {
-		// list.unshift(input);
+	const handleChange = (e) => {
+		setInput(e.target.value);
+	};
+	const handleClick = (e) => {
+		e.preventDefault();
 		addItem([...list, input]);
 		setInput('');
-	}
-  return (
-    <div>
-      <h3>add new to-do</h3>
-      <form>
-        <label htmlFor='input'>Enter Text</label>
-        <input id='input' onChange={handleChange} />
+	};
+	return (
+		<div>
+			<h3>add new to-do</h3>
+			<form onSubmit={handleClick}>
+				<label htmlFor='input'>Enter Text</label>
+				<input id='input' value={input} onChange={handleChange} />
 				<button onClick={handleClick}>submit</button>
-      </form>
-    </div>
-  );
+			</form>
+		</div>
+	);
 };
 
 export default Form;
--- src/form.test.js
+++ src/form.test.js
@@ -1,32 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import configureMockStore from "redux-mock-store";
+// import ReactDOM from 'react-dom';
+// import configureMockStore from "redux-mock-store";
 import { render, fireEvent } from 'react-testing-library';
 import Form from './components/form';
-import { addTodo } from './redux/actions';
+// import { addTodo } from './redux/actions';
 
 // beforeEach(render(<From />));
 
 describe('form', () => {
-  it("renders input for new to-do", () => {
-    const { getByLabelText } = render(<Form />);
-    const input = getByLabelText(/enter/i);
-    expect(input).toBeDefined();
-    expect(input.value).toBe('');
-    fireEvent.change(input, { target: { value: 'new' } });
-    expect(input.value).toBe('new');
-  });
-  it("submit button clears input and calls \'addTodo\' action creator", () => {
-    const { getByLabelText, getByText } = render(<Form />);
-    const input = getByLabelText(/enter/i);
-    const submitButton = getByText(/submit/i);
-    expect(input).toBeDefined();
-    expect(submitButton).toBeDefined();
-    expect(input.value).toBe('');
-    fireEvent.change(input, { target: { value: 'new' } });
-    expect(handleChange).toBeCalledTimes(1);
-    expect(input.value).toBe('new');
-    fireEvent.click(submitButton);
-    expect(handleClick).toBeCalledTimes(1);
-  })
+	it('renders input for new to-do', () => {
+		const { getByLabelText } = render(<Form />);
+		const input = getByLabelText(/enter/i);
+		expect(input).toBeDefined();
+		expect(input.value).toBe('');
+		fireEvent.change(input, { target: { value: 'new' } });
+		expect(input.value).toBe('new');
+	});
+	xit("submit button clears input and calls 'addTodo' action creator", () => {
+		const { getByLabelText, getByText } = render(<Form />);
+		const input = getByLabelText(/enter/i);
+		const submitButton = getByText(/submit/i);
+		expect(input).toBeDefined();
+		expect(submitButton).toBeDefined();
+		expect(input.value).toBe('');
+		fireEvent.change(input, { target: { value: 'new' } });
+		expect(input.value).toBe('new');
+		fireEvent.click(getByText('submit'));
+		expect(input.value).toBe('');
+	});
+	it("submit button clears input and callls onSubmit", () => {
+		const { getByLabelText, getByText } = render(<Form />);
+		const input = getByLabelText(/enter/i);
+		const submitButton = getByText(/submit/i);
+		const handleClick = jest.fn();
+		const onSubmit = jest.fn();
+		expect(input).toBeDefined();
+		expect(submitButton).toBeDefined();
+		expect(input.value).toBe('');
+		fireEvent.change(input, { target: { value: 'new' } });
+		expect(input.value).toBe('new');
+		fireEvent.click(getByText('submit'));
+		expect(input.value).toBe('');
+		expect(handleClick).toBeCalled();
+		expect(onSubmit).toBeCalled();
+	});
 });
